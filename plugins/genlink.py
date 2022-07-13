99299d8d9dc7a22b3c447bbaad19d1ef79c6898b
supersip <108521768+supersip@users.noreply.github.com>
2022-07-13 19:00:35 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -67,7 +67,7 @@ async def gen_link_batch(bot, message):
     try:
         chat_id = (await bot.get_chat(f_chat_id)).id
     except ChannelInvalid:
-        return await message.reply('This may be a private channel / group. Make me an admin over there to index the files.')
+        return await message.reply('This may be a private channel / group. Make me an admin over there to index the files😇.')
     except (UsernameInvalid, UsernameNotModified):
         return await message.reply('Invalid Link specified.')
     except Exception as e:
